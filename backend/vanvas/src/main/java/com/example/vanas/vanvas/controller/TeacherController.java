4a186ddfceba3a2f820cd4e97e9bc7f018f9e9c2
EP0923 <146797482+ElmerPonce07@users.noreply.github.com>
2024-11-25 23:01:35 -0500

    added delete and add to classroom
    added student and teacher controller to a copy of the current main

---
+++ backend/vanvas/src/main/java/com/example/vanas/vanvas/controller/TeacherController.java
@@ -0,0 +1,29 @@
+package com.example.vanas.vanvas.controller;
+
+import com.example.vanas.vanvas.model.Teacher;
+import com.example.vanas.vanvas.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class TeacherController {
+    private final TeacherService teacherService;
+
+    @Autowired
+    public TeacherController (TeacherService teacherService){
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerTeacher (@RequestBody Teacher teacher){
+        try {
+            teacherService.registerTeacher(teacher);
+            return new ResponseEntity<>("Teacher registered successfully", HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}

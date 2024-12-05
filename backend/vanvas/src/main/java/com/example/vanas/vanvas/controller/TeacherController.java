9467f7204871faaf11eae79bcfa89e087d969023
Andro Aguilar <androdavidaguilar@gmail.com>
2024-12-05 06:49:51 -0500

    added Dashboard, Profile, and Register pages. Modified Sidebar. added port for backend. Some of the uploaded code is Sam's.

--- backend/vanvas/src/main/java/com/example/vanas/vanvas/controller/TeacherController.java
+++ backend/vanvas/src/main/java/com/example/vanas/vanvas/controller/TeacherController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
 @RestController
 @RequestMapping
 public class TeacherController {

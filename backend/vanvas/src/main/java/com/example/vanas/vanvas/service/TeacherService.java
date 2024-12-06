9e43598611aace6e225949237b6c28e3cd8785a2
Andro Aguilar <androdavidaguilar@gmail.com>
2024-12-05 22:24:16 -0500

    reverting backend

--- backend/vanvas/src/main/java/com/example/vanas/vanvas/service/TeacherService.java
+++ backend/vanvas/src/main/java/com/example/vanas/vanvas/service/TeacherService.java
@@ -1,17 +1,23 @@
 package com.example.vanas.vanvas.service;
 
+import com.example.vanas.vanvas.model.Classroom;
+import com.example.vanas.vanvas.model.Teacher;
+import com.example.vanas.vanvas.repository.ClassroomRepo;
+import com.example.vanas.vanvas.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.example.vanas.vanvas.model.Teacher;
-import com.example.vanas.vanvas.repository.TeacherRepository;
+import java.util.List;
 
 @Service
 public class TeacherService {
 
     private final TeacherRepository teacherRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ClassroomRepo classroomRepo;
 
     @Autowired
     public TeacherService(TeacherRepository teacherRepository, BCryptPasswordEncoder passwordEncoder) {
@@ -42,5 +48,16 @@ public class TeacherService {
                 .orElseThrow(() -> new IllegalArgumentException("Teacher with email not found"));
     }
 
+    public Teacher getTeacherById(String teacherId) {
+        return teacherRepository.findById(teacherId)
+                .orElseThrow(() -> new IllegalArgumentException("Teacher not found with ID: " + teacherId));
+    }
 
+
+
+    public List<Classroom> getClassesByTeacherId(String teacherId) {
+        Teacher teacher = teacherRepository.findById(teacherId)
+                .orElseThrow(() -> new IllegalArgumentException("Teacher not found with ID: " + teacherId));
+        return classroomRepo.findByTeacherId(teacher);
+    }
 }
